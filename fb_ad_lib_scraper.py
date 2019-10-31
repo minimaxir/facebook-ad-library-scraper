8ae700c36b8968d685011a6fca0e7f67035fee47
Max Woolf <max@minimaxir.com>
2019-10-30 20:12:24 -0700

    Write unnested CSVs

--- fb_ad_lib_scraper.py
+++ fb_ad_lib_scraper.py
@@ -24,6 +24,16 @@ w1 = csv.DictWriter(f1, fieldnames=config['output_fields'],
                     extrasaction='ignore')
 w1.writeheader()
 
+f2 = open('fb_ads_demos.csv', 'w')
+w2 = csv.DictWriter(f2, fieldnames=config['demo_fields'],
+                    extrasaction='ignore')
+w2.writeheader()
+
+f3 = open('fb_ads_regions.csv', 'w')
+w3 = csv.DictWriter(f3, fieldnames=config['region_fields'],
+                    extrasaction='ignore')
+w3.writeheader()
+
 pbar = tqdm(total=config['search_total'], smoothing=0)
 
 for _ in range(int(config['search_total'] / config['page_total'])):
@@ -36,6 +46,16 @@ for _ in range(int(config['search_total'] / config['page_total'])):
 
         ad_id = re.search(r'\d+', ad['ad_snapshot_url']).group(0)
         ad_url = 'https://www.facebook.com/ads/library/?id=' + ad_id
+
+        # write to the unnested files
+        for demo in ad['demographic_distribution']:
+            demo.update({'ad_id': ad_id})
+            w2.writerow(demo)
+
+        for region in ad['region_distribution']:
+            region.update({'ad_id': ad_id})
+            w3.writerow(region)
+
         ad.update({'ad_id': ad_id,
                    'ad_url': ad_url,
                    'impressions_min': ad['impressions']['lower_bound'],
@@ -43,9 +63,12 @@ for _ in range(int(config['search_total'] / config['page_total'])):
                    'spend_min': ad['spend']['lower_bound'],
                    'spend_max': ad['spend']['upper_bound'],
                    })
+
         w1.writerow(ad)
         pbar.update()
     params.update({'after': data['paging']['cursors']['after']})
 
 f1.close()
+f2.close()
+f3.close()
 pbar.close()
