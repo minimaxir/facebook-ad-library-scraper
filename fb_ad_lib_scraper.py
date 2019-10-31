a54388114b7205bf3744cbac4ed5db230320195d
Max Woolf <max@minimaxir.com>
2019-10-30 19:48:41 -0700

    pagenation + progress bar

--- fb_ad_lib_scraper.py
+++ fb_ad_lib_scraper.py
@@ -2,9 +2,13 @@ import yaml
 import requests
 import csv
 import re
+from tqdm import tqdm
 
 with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
+
+assert config['search_total'] % config['page_total'] == 0, \
+    "search_total should be a multiple of page_total."
 
 params = {
     'access_token': config['access_token'],
@@ -16,22 +20,27 @@ params = {
 }
 
 f1 = open('fb_ads.csv', 'w')
-
 w1 = csv.DictWriter(f1, fieldnames=config['output_fields'],
                     extrasaction='ignore')
 w1.writeheader()
 
-r = requests.get('https://graph.facebook.com/v5.0/ads_archive',
-                 params=params)
-data = r.json()
-for ad in data['data']:
-    # The ad_id is encoded in the ad snapshot URL
-    # and cannot be accessed as a normal field. (?!?!)
+pbar = tqdm(total=config['search_total'], smoothing=0)
 
-    ad_id = re.search(r'\d+', ad['ad_snapshot_url']).group(0)
-    ad_url = 'https://www.facebook.com/ads/library/?id=' + ad_id
-    ad.update({'ad_id': ad_id,
-               'ad_url': ad_url})
-    w1.writerow(ad)
+for _ in range(int(config['search_total'] / config['page_total'])):
+    r = requests.get('https://graph.facebook.com/v5.0/ads_archive',
+                     params=params)
+    data = r.json()
+    for ad in data['data']:
+        # The ad_id is encoded in the ad snapshot URL
+        # and cannot be accessed as a normal field. (?!?!)
+
+        ad_id = re.search(r'\d+', ad['ad_snapshot_url']).group(0)
+        ad_url = 'https://www.facebook.com/ads/library/?id=' + ad_id
+        ad.update({'ad_id': ad_id,
+                   'ad_url': ad_url})
+        w1.writerow(ad)
+        pbar.update()
+    params.update({'after': data['paging']['cursors']['after']})
 
 f1.close()
+pbar.close()
