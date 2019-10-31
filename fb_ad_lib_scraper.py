6dfe2d6c523b2aafa6299b87861dd1b06d1544d2
Max Woolf <max@minimaxir.com>
2019-10-30 18:42:36 -0700

    Write to CSV

--- fb_ad_lib_scraper.py
+++ fb_ad_lib_scraper.py
@@ -1,21 +1,26 @@
 import yaml
 import requests
+import csv
 
 with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
-
-query_fields = config['query_fields']
 
 params = {
     'access_token': config['access_token'],
     'ad_type': 'POLITICAL_AND_ISSUE_ADS',
     'ad_reached_countries': "['US']",
-    'search_terms': config['search_terms'],
-    'fields': ",".join(query_fields),
+    'search_terms': config.get('search_terms'),
+    'fields': ",".join(config['query_fields']),
     'limit': config['page_total']
 }
 
-r = requests.get('https://graph.facebook.com/v5.0/ads_archive', params=params)
-data = r.json()
+with open('fb_ads.csv', 'w') as f1:
+    w1 = csv.DictWriter(f1, fieldnames=config['output_fields'],
+                        extrasaction='ignore')
+    w1.writeheader()
 
-print(data)
+    r = requests.get('https://graph.facebook.com/v5.0/ads_archive',
+                     params=params)
+    data = r.json()
+    for ad in data['data']:
+        w1.writerow(ad)
